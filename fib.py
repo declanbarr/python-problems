9bcef983c7a3b6863b411f4a742edb5cdd498104
declanbarr <35577478+declanbarr@users.noreply.github.com>
2018-02-07 17:59:23 +0000

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,4 +1,4 @@
-# Declan Barr 07 Jan 2018
+# Declan Barr 07 Feb 2018
 # My name is Declan, so the first and last letter of my name (D + N = 4 + 14) give the number  18. The 18th Fibonacci number is 2,584.
 # Adapted from https://github.com/ianmcloughlin/python-fib/blob/master/fib.py
 # A program that displays Fibonacci numbers.
