db35885a0c485a7c65eff1351ebee2ef5f185343
declanbarr <declan.barr@outlook.com>
2018-02-27 21:40:36 +0000

    changed start for i to 0, removed i =20

--- euler5.py
+++ euler5.py
@@ -1,13 +1,12 @@
 # Declan Barr 13 Feb 2018
 # Euler problem 5
 
-i = 20
 
 
 numberFound = False
 
 while numberFound != True:
-    for i in range(1, 1000000000000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
+    for i in range(0, 1000000000000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
         for x in range(1, 21):
             print("i is :", i, "x is :", x)
             if i % x == 0:
