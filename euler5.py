2579bfa053b4100788356afd42d44ecc215d280a
declanbarr <declan.barr@outlook.com>
2018-02-27 21:51:08 +0000

    changed lower bound for x to 11

--- euler5.py
+++ euler5.py
@@ -7,7 +7,7 @@ numberFound = False
 
 while numberFound != True:
     for i in range(0, 1000000000000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
-        for x in range(1, 21):
+        for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
             print("i is :", i, "x is :", x)
             if i % x == 0:
                 numberFound = True
