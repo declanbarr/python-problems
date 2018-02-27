4308ef21528826e6d519f2ab41cf086772abecdf
declanbarr <declan.barr@outlook.com>
2018-02-27 22:03:26 +0000

    Testing if while loop is working as intended

--- euler5.py
+++ euler5.py
@@ -4,7 +4,8 @@
 numberFound = False
 
 while numberFound != True:
-    for i in range(0, 1000000000000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
+    # Testing to see if program able to finish when numberFound = true 
+    for i in range(232790000, 232793000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
         for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
             print("i is :", i, "x is :", x)
             if i % x == 0:
