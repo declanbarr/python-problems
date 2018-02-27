c3d6dc9ae3f47ed791112b05dd48546b1eb10521
declanbarr <declan.barr@outlook.com>
2018-02-27 23:01:09 +0000

    2nd last digit produced is the correct answer. Needs a little refinement

--- euler5.py
+++ euler5.py
@@ -1,18 +1,19 @@
 # Declan Barr 13 Feb 2018
 # Euler problem 5
-numberFound = false
+numberFound = False
 
 for i in range(232790000, 232793000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
 
-    while numberFound == false:
-    for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
-        print("i is :", i, "x is :", x)
-        if i % x == 0:
-            print i
-            numberFound = True
-            break
-        else:
-            break
-    break            
+    while numberFound == False:
+        for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
+            print("i is :", i, "x is :", x)
+            if i % x == 0:
+                print(i)
+                if x == 20:
+                    numberFound = True
+                    break
+            else:
+                break
+        break            
             
 print(i)
