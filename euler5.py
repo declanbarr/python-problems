0c7632892152f109a263153b31f08fab9ff0597c
declanbarr <declan.barr@outlook.com>
2018-02-27 22:57:41 +0000

    Moved while loop inside for i loop

--- euler5.py
+++ euler5.py
@@ -1,17 +1,18 @@
 # Declan Barr 13 Feb 2018
 # Euler problem 5
+numberFound = false
 
-numberFound = False
+for i in range(232790000, 232793000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
 
-while numberFound != True:
-    # Testing to see if program able to finish when numberFound = true 
-    for i in range(232790000, 232793000, 20): #i can increment in 20 as all the numbers need to be divisable by 20
-        for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
-            print("i is :", i, "x is :", x)
-            if i % x == 0:
-                numberFound = True # This does not exit the while loop as intended
-            else:
-                break
-                
+    while numberFound == false:
+    for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
+        print("i is :", i, "x is :", x)
+        if i % x == 0:
+            print i
+            numberFound = True
+            break
+        else:
+            break
+    break            
             
 print(i)
