f5fc5e905e14a187f6fc8cfeb81d66acef9fc430
declanbarr <declan.barr@outlook.com>
2018-02-28 00:25:13 +0000

    commonMultiple variable added

--- euler5.py
+++ euler5.py
@@ -2,7 +2,9 @@
 # Euler problem 5
 numberFound = False
 
-for i in range(2520, 232793000, 2520): #Lower bound can start at 2620 as this is the LCM of 1 to 10. The step can also be 2520
+commonMultiple = 2520 * 11 * 12 * 13 * 14 * 15 * 16 * 17 * 18 * 19 * 20 # 2520 is the lcm of 1 to 10
+
+for i in range(2520, commonMultiple, 2520): #Lower bound can start at 2620 as this is the LCM of 1 to 10. The step can also be 2520
 
     while numberFound == False:
         for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
