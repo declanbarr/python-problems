1ae114f0e2f9f805736abbc28d998abf923ae65b
declanbarr <declan.barr@outlook.com>
2018-02-28 12:34:18 +0000

    Program simplified to contain a single for loop

--- euler5.py
+++ euler5.py
@@ -1,17 +1,17 @@
 # Declan Barr 13 Feb 2018
 # Euler problem 5
 numberFound = False
+i = 2520
 
 while numberFound == False:
     for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
         if i % x == 0:
-            if x == 20:
+            x = x + 1
+            if  x == 21:
                 numberFound = True
-                break
         else:
+            i = i + 2520
             break
-    break
-if numberFound == True:
-    break
+    x = x + 1
                
 print(i)
