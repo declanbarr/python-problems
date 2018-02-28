753e1e1ba5e5edbdbcbead8288985b995df147a6
declanbarr <declan.barr@outlook.com>
2018-02-28 00:21:01 +0000

    Unnessary print statements removed

--- euler5.py
+++ euler5.py
@@ -6,9 +6,7 @@ for i in range(2520, 232793000, 2520): #Lower bound can start at 2620 as this is
 
     while numberFound == False:
         for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
-            print("i is :", i, "x is :", x)
             if i % x == 0:
-                print(i)
                 if x == 20:
                     numberFound = True
                     break
