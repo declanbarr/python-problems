705cf4461df9b8c201dadde7f721e6a95ef75b0a
declanbarr <declan.barr@outlook.com>
2018-03-06 21:43:44 +0000

    Comments added

--- irisdata.py
+++ irisdata.py
@@ -1,3 +1,8 @@
+# Declan Barr 06 Mar 2018
+# Script that reads the Iris data set in and prints the four numerical values 
+# on each row
+# Line 9 Adapted from https://pyformat.info/
+
 with open("data/iris.csv") as f:
     for line in f:
         contents = line.split(',')
