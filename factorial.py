c31e5959671cc1fe02626b20b56be39b9d37ea1c
declanbarr <declan.barr@outlook.com>
2018-03-16 20:45:32 +0000

    Script produces correct output

--- factorial.py
+++ factorial.py
@@ -4,9 +4,18 @@
 def factorial(factstart):
     y = factstart
     for x in range(factstart -1, 1, -1):
-        print(x, y)
         y = x * y
-        print(x, y)
     return y
 
 print("The factorial of 5 is : ", factorial(5))
+print("The factorial of 7 is : ", factorial(7))
+print("The factorial of 10 is : ", factorial(10))
+
+
+factcheck10 = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+factcheck7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+factcheck5 = 5 * 4 * 3 * 2 * 1
+
+print("The factorial of 5 is : ", factcheck5)
+print("The factorial of 7 is : ", factcheck7)
+print("The factorial of 10 is : ", factcheck10)
