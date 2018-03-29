1ac7a98ba13d84e642bbfc98d2d04f1327eb84d9
Declan Barr <deckybarr@googlemail.com>
2018-03-29 20:19:47 +0100

    Code produces correct output. Upper bound changed to 20 Factorial

--- euler5.py
+++ euler5.py
@@ -2,10 +2,12 @@
 # Euler problem 5 from Project Euler (https://projecteuler.net/problem=5)
 # Idea to set i to an initial value of 2520 and incrementing by 2520 found on
 #  https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
+import math
+twentyFact = math.factorial(20)
 
 numberFound = False
 
-for i in range(2520, 10000000000000, 2520):
+for i in range(2520, twentyFact, 2520):
     for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
                             # eg 20 = 2 * 10 therefore if a number is divisable by 20
                             # it is also divisable by 2 and 10
