92666b117c4d179f5359be646ccb3bd87df04955
Declan Barr <deckybarr@googlemail.com>
2018-03-29 20:38:27 +0100

    Comments added

--- euler5.py
+++ euler5.py
@@ -3,20 +3,21 @@
 # Idea to set i to an initial value of 2520 and incrementing by 2520 found on
 #  https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
 import math
-twentyFact = math.factorial(20)
+twentyFact = math.factorial(20)  # This will be used as the upper bound for the "For i" loop
 
-numberFound = False
+numberFound = False # numberFound will be set to true when the lcm of the number 1 to 20 has been found
 
 for i in range(2520, twentyFact, 2520):
     for x in range(11, 21): # numbers from 1 to 10 don't need to be checked as numbers 11 to 20 are multiples of these
                             # eg 20 = 2 * 10 therefore if a number is divisable by 20
                             # it is also divisable by 2 and 10
-        if i % x == 0:      
+ 
+        if i % x == 0:  # This checks if i is evenly divisible by x     
             if x == 20:   
                 numberFound = True
         else:
-            break
-    if numberFound == True:
-        break
+            break # This breaks out of the "for x loop
+    if numberFound == True: 
+        break # This breaks out of the "for i" loop
                
 print(i)
