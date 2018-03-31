b8247ddfb69f8c94b1d0bad541faed65e0b43776
Declan Barr <deckybarr@googlemail.com>
2018-03-31 13:08:03 +0100

    Comment added

--- factorial.py
+++ factorial.py
@@ -1,10 +1,10 @@
 # Declan Barr 16 Mar 2018
 # Script that uses a function to calculate the factorial of a number
 
-def factorial(factstart):
+def factorial(factstart): 
     factAns = factstart # factAns is assigned the number for which you wish to find the factorial
     for x in range(factstart -1, 1, -1): # The for loop provides all the number below the number 
-                                         # you wish to find the factorial of
+                                         # you wish to find the factorial of (below factstart)
                                          # For loop increments down rather than up
         factAns = factAns * x        
     return factAns
