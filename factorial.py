9dba3f62f6d26ed59d7d604adc4db790f57f93d4
Declan Barr <deckybarr@googlemail.com>
2018-03-31 12:13:00 +0100

    Removed unnessary white space and duplicated code

--- factorial.py
+++ factorial.py
@@ -2,15 +2,12 @@
 # Script that uses a function to calculate the factorial of a number
 
 def factorial(factstart):
-    def factorial(factstart):
     for x in range(factstart, 1, -1):
         print(x)
         x = x * (x - 1)
         print(x)
     return x
  
-
-
 print("The factorial of 5 is : ", factorial(5))
 print("The factorial of 7 is : ", factorial(7))
 print("The factorial of 10 is : ", factorial(10))
